c8913e53c5e726d370174b9606cfab8c789c26df
Eduardo <temporariac432@gmail.com>
2025-02-10 21:33:16 -0300

    settings atualizado

--- quickhost/settings.py
+++ quickhost/settings.py
@@ -12,6 +12,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "rest_framework",
     "corsheaders",
     "data",
